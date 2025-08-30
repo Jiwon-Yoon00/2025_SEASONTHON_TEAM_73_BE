19408ce590dd4bf208c88d317f6de764c4baca71
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-08-31 00:28:49 +0900

    [ADD/#1] 스웨거에서 JWT사용 가능하도록 코드 추가

--- src/main/java/com/season/livingmate/config/SwagerConfig.java
+++ src/main/java/com/season/livingmate/config/SwagerConfig.java
@@ -1,5 +1,8 @@
 package com.season.livingmate.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +14,27 @@ public class SwagerConfig {
 
 	@Bean
 	public OpenAPI customOpenAPI() {
+
+		String jwtSchemeName = "JWT";
+
+		// Security Requirement 설정
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+		// Security Scheme 설정 (헤더에 Authorization: Bearer {token})
+		Components components = new Components()
+				.addSecuritySchemes(jwtSchemeName,
+						new SecurityScheme()
+								.name(jwtSchemeName)
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT"));
+
 		return new OpenAPI()
 			.info(new Info()
 				.title("Livingmate API")
-				.description("Livingmate REST API 문서입니다."))
-
-			;
-
+				.description("Livingmate REST API 문서입니다.")
+			)
+				.addSecurityItem(securityRequirement)
+				.components(components);
 	}
 }
