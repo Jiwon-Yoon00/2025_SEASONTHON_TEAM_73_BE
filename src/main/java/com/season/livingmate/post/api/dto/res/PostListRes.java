8917167774efe9b1cbd952e3047b5dba51a7a536
daun <wjdekdns0218@gmail.com>
2025-09-04 19:35:45 +0900

    [MOD/#20] PostListRes 이미지, 룸타입, 보증금, 월세, 성별 추가

--- src/main/java/com/season/livingmate/post/api/dto/res/PostListRes.java
+++ src/main/java/com/season/livingmate/post/api/dto/res/PostListRes.java
@@ -1,6 +1,7 @@
 package com.season.livingmate.post.api.dto.res;
 
 import com.season.livingmate.post.domain.Post;
+import com.season.livingmate.user.domain.Gender;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
@@ -9,17 +10,32 @@ public record PostListRes(
 
         Long id,
 
+        String imageUrl,
+
         String title,
 
+        String roomType,
+
+        int deposit,
+
+        int monthlyRent,
+
         String region,
+
+        Gender gender,
 
         LocalDateTime availableDate
 ) {
         public static PostListRes from(Post p) {
                 return new PostListRes(
                         p.getPostId(),
+                        p.getImageUrl(),
                         p.getTitle(),
+                        p.getRoomType().getKoreanName(),
+                        p.getDeposit(),
+                        p.getMonthlyRent(),
                         p.getRegionLabel(),
+                        p.getUser().getGender(),
                         p.getAvailableDate());
         }
 }
