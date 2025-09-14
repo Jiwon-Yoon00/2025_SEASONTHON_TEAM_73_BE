783b69c226698059f76dbfd2d9d08f5512b4e995
daun <wjdekdns0218@gmail.com>
2025-09-15 00:29:40 +0900

    [FEAT/#64] 방 유무에 따른 사용자 조회 추가 및 errorStatus

--- src/main/java/com/season/livingmate/user/domain/repository/UserRepository.java
+++ src/main/java/com/season/livingmate/user/domain/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.season.livingmate.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUsername(String username);
     Optional<User> findByUsername(String username);
 
+    // isRoom 조건에 따른 사용자 조회
+    List<User> findByIsRoomAndIdNot(Boolean isRoom, Long id);
 }
