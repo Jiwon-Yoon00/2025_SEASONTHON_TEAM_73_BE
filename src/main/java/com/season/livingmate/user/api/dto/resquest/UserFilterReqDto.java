7c4498344812b9198daa76fbc39b25dd0b611b85
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-09-15 17:29:01 +0900

    [FEAT/#67] 유저 필터 복수 선택 가능하도록 변경

--- src/main/java/com/season/livingmate/user/api/dto/resquest/UserFilterReqDto.java
+++ src/main/java/com/season/livingmate/user/api/dto/resquest/UserFilterReqDto.java
@@ -2,16 +2,27 @@ package com.season.livingmate.user.api.dto.resquest;
 
 import com.season.livingmate.user.domain.CountRange;
 import com.season.livingmate.user.domain.SensitivityLevel;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@Schema(description = "사용자 필터 요청 DTO")
 public class UserFilterReqDto {
-    private boolean smoking; // 흡연 여부
-    private CountRange alcoholCount; // 음주 횟수
-    private SensitivityLevel sleepLevel; // 잠귀 민감도
-    private List<String> pet;
-    private SensitivityLevel tidinessLevel; // 정리정돈 성향
 
+    @Schema(description = "흡연 여부", example = "false")
+    private boolean smoking; // 흡연 여부
+
+    @Schema(description = "음주 횟수", example = "[\"ZERO\"]")
+    private List<CountRange> alcoholCount; // 음주 횟수
+
+    @Schema(description = "잠귀 민감도", example = "[\"LOW\", \"MEDIUM\"]")
+    private List<SensitivityLevel> sleepLevel; // 잠귀 민감도
+
+    @Schema(description = "반려동물", example = "[\"강아지\", \"고양이, \"물고기\"]")
+    private List<String> pet;
+
+    @Schema(description = "정리정돈 성향", example = "[\"LOW\", \"MEDIUM\"]")
+    private List<SensitivityLevel> tidinessLevel; // 정리정돈 성향
 }
